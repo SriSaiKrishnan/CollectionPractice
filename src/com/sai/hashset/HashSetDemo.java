71dd902a00b4cc83e7f1922f97d44d6fff61bdba
Srisaikrishnan V <sv@SVMBP18.local>
2020-01-29 20:26:52 +0530

    Second Commit

--- src/com/sai/hashset/HashSetDemo.java
+++ src/com/sai/hashset/HashSetDemo.java
@@ -3,6 +3,8 @@
  */
 package com.sai.hashset;
 
+import java.util.HashSet;
+
 /**
  * @author sv
  *
@@ -14,6 +16,8 @@ public class HashSetDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		HashSet<String> hs = new HashSet<String>();
 
 	}
 
